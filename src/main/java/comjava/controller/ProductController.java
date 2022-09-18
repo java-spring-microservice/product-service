69c535b1e69934ceda42a5752e8d3bc782004a24
Tiên Huỳnh <52915164+anhtienzz123@users.noreply.github.com>
2022-09-18 23:02:24 +0700

    update

--- src/main/java/comjava/controller/ProductController.java
+++ src/main/java/comjava/controller/ProductController.java
@@ -3,6 +3,7 @@ package comjava.controller;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import comjava.entity.Product;
@@ -18,5 +19,10 @@ public class ProductController {
     @GetMapping
     public List<Product> getProductList() {
         return productRepository.findAll();
+    }
+
+    @GetMapping("/{productId}")
+    public Product getProductDetail(@PathVariable Long productId) {
+        return productRepository.findById(productId).orElse(null);
     }
 }
