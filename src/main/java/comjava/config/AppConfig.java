98b4f300e7dbd058ca8f214cce5a3c48d8e1d008
Tien Huynh Anh <hatien@tma.com.vn>
2022-09-19 17:27:55 +0700

    update

---
+++ src/main/java/comjava/config/AppConfig.java
@@ -0,0 +1,14 @@
+package comjava.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import brave.sampler.Sampler;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    Sampler sampler() {
+        return Sampler.ALWAYS_SAMPLE;
+    }
+}
--- src/main/java/comjava/controller/ProductController.java
+++ src/main/java/comjava/controller/ProductController.java
@@ -1,6 +1,8 @@
 package comjava.controller;
 
 import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,16 +15,29 @@ import comjava.repository.ProductRepository;
 @RequestMapping("/products")
 public class ProductController {
 
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+
     @Autowired
     private ProductRepository productRepository;
 
     @GetMapping
     public List<Product> getProductList() {
+
+        logger.info("getProductList");
         return productRepository.findAll();
     }
 
     @GetMapping("/{productId}")
     public Product getProductDetail(@PathVariable Long productId) {
         return productRepository.findById(productId).orElse(null);
+    }
+
+    @GetMapping("/test/exception")
+    public Product testException() {
+        throw new RuntimeException("Exception");
+    }
+
+    public Product getProductDefault() {
+        return Product.builder().id(0L).name("default").price(0.0).quantity(0L).build();
     }
 }
